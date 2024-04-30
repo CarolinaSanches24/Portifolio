97a8cb4d6141f3e1d6b1c95be927ec0f2566e6c3
CAROL SANCHES <135077050+CarolinaSanches24@users.noreply.github.com>
2024-04-30 16:43:42 -0300

    feat: add project

--- my_portifolio/src/components/Projects/Projects.tsx
+++ my_portifolio/src/components/Projects/Projects.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import styles from "./Projects.module.css";
+import projects from "../../data/projects.json";
+import { getImageUrl } from '../../utils';
 
 export const Projects = () => {
     return (
       <section id="projects" className={styles.container_projects}>
-      
+      <h2 className={styles.title}>Projects</h2>
+      <div className={styles.cards}>
+      {projects.map((project) => {
+        return(
+          <div key = {project.id} className={styles.card}>
+            <figure>
+            <img src ={getImageUrl(project.imageSrc)} alt ={project.title}/>
+            </figure>
+            <figcaption>
+              <h1>{project.title}</h1>
+              <p>{project.description}</p>
+            </figcaption>
+            </div>
+        )
+      })}
+
+      </div>
       </section>
     );
   };
